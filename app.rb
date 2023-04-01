fe9356c2df76e18b0d56f53d42153377b8110b95
jesse <jesse.kibaara@student.moringaschool.com>
2023-04-01 16:21:05 +0300

    challege-1 code written

---
+++ app.rb
@@ -0,0 +1,17 @@
+#Challenge 1: No ifs no buts(Toy Problem)
+
+def compare(a, b)
+    result = "#{a} is equal to #{b}"
+    result = "#{a} is greater than #{b}" * (a > b ? 1 : 0)
+    result = "#{a} is smaller than #{b}" * (a < b ? 1 : 0)
+    result
+  end
+
+  #Challenge 2: Ordered Count of Characters(Toy Problem)
+
+  def ordered_count(str)
+    counts = Hash.new(0)
+    str.chars.each { |char| counts[char] += 1 }
+    counts.map { |k, v| [k, v] }
+  end
+  
